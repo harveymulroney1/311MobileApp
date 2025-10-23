1ce5fd7da7d13d61b47fd73ee304f1993cff85fb
harveymulroney1 <harveymulroney@gmail.com>
2025-10-23 18:29:01 +0100

    Light Control

--- app/DeviceManager.tsx
+++ app/DeviceManager.tsx
@@ -1,16 +1,64 @@
+import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 export default function deviceManager() {
     const [lastUpdated,setlastUpdated] = useState("");
     const [batteryPercentage,setbatteryPercentage] = useState("");
     
+    const host = '192.168.4.1';
+    const port = 3000;
+    const [redStatus,setredStatus] = useState(Boolean);
+    const [greenStatus,setgreenStatus] = useState(Boolean);
+    const [blueStatus,setblueStatus] = useState(Boolean);
+
     useEffect(()=>
     {
         setlastUpdated("14 Minutes Ago");
         setbatteryPercentage("53%");
     }
     ,[])
-    
+    const toggleRed = ( ()=> {
+        if(redStatus == false)
+        {
+            axios.get("http://" + host +"/redON")
+            console.log("Setting Red ON");
+            setredStatus(true);
+        }
+        else{
+            axios.get("http://" + host + ":" + port+"/redOFF")
+            console.log("Setting Red OFF");
+            setredStatus(false);
+        }
+    }
+    );
+    const toggleGreen = ( ()=> {
+        if(greenStatus == false)
+        {
+            axios.get("http://" + host +"/greenON")
+            console.log("Setting Red ON");
+            setredStatus(true);
+        }
+        else{
+            axios.get("http://" + host +"/greenOFF")
+            console.log("Setting Red OFF");
+            setredStatus(false);
+        }
+    }
+    );
+        const toggleBlue = ( ()=> {
+        if(blueStatus == false)
+        {
+            axios.get("http://" + host +"/blueON")
+            console.log("Setting Red ON");
+            setredStatus(true);
+        }
+        else{
+            axios.get("http://" + host +"/blueOFF")
+            console.log("Setting Red OFF");
+            setredStatus(false);
+        }
+    }
+    );
     return (
         
         <View>
@@ -19,6 +67,12 @@ export default function deviceManager() {
             <Text>Last Updated: {lastUpdated}</Text>
             <View style={styles.btnContainer}>
                 <TouchableOpacity style={styles.safeModeBtn}>Safe Mode</TouchableOpacity>
+            </View>
+            <View>
+                <Text>Light Controller</Text>
+                <TouchableOpacity
+                onPress={()=>toggleRed()}
+                >{redStatus?"Red On":"Red Off"}</TouchableOpacity>
             </View>
         </View>
     )
